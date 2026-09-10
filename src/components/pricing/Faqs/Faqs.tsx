d3cd50c0e9cc516f8b79a1e8fdee7a7ff3c7316c
wafaahegazy77 <wafaahegazy7720@gmail.com>
2026-09-10 17:23:08 +0300

    edit  in faq sec

--- src/components/pricing/Faqs/Faqs.tsx
+++ src/components/pricing/Faqs/Faqs.tsx
@@ -19,21 +19,18 @@ const Faqs = async () => {
     return (
         <section className="faq pb-120">
             <div className="container">
-                <div className="row align-items-start">
+                <div className="row">
 
                     <div className="col-lg-3">
                         <div className="faq-title col-lg-6">
-                            <h2 className="fsz-40 fw-800 color_primary mb-0">
+                            <h2 className="fsz-40 fw-600 color_primary mb-0">
                                 {t("title")}
                             </h2>
                         </div>
                     </div>
 
                     <div className="col-lg-9">
-                        <div
-                            className="accordion"
-                            id="faqAccordion"
-                        >
+                        <div className="accordion" id="faqAccordion">
                             {faqs.map((faq, index) => {
                                 const faqId = `faq-${faq.code}`;
 
@@ -45,16 +42,12 @@ const Faqs = async () => {
                                         <h2 className="accordion-header">
                                             <button
                                                 className={`accordion-button fsz-16 fw-700 ${
-                                                    index !== 0
-                                                        ? "collapsed"
-                                                        : ""
+                                                    index !== 0 ? "collapsed" : ""
                                                 }`}
                                                 type="button"
                                                 data-bs-toggle="collapse"
                                                 data-bs-target={`#${faqId}`}
-                                                aria-expanded={
-                                                    index === 0
-                                                }
+                                                aria-expanded={index === 0}
                                                 aria-controls={faqId}
                                             >
                                                 <span className="ico radius-100 dnf-center me-15">
@@ -71,9 +64,7 @@ const Faqs = async () => {
                                         <div
                                             id={faqId}
                                             className={`accordion-collapse collapse ${
-                                                index === 0
-                                                    ? "show"
-                                                    : ""
+                                                index === 0 ? "show" : ""
                                             }`}
                                             data-bs-parent="#faqAccordion"
                                         >
